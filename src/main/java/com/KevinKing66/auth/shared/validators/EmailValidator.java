a56b42aa7d95dbc187f327878754c55c388ac2d2
KevinKing66 <kevinvallenato2002@gmail.com>
2024-12-28 12:12:59 -0500

    Se eliminan warning de decodificación

--- src/main/java/com/KevinKing66/auth/shared/validators/EmailValidator.java
+++ src/main/java/com/KevinKing66/auth/shared/validators/EmailValidator.java
@@ -11,30 +11,28 @@ import java.util.regex.Pattern;
  * @author kevin
  */
 public class EmailValidator {
-    
 
-    // Expresión regular para validar emails
     private static final String EMAIL_REGEX = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
     private static final Pattern EMAIL_PATTERN = Pattern.compile(EMAIL_REGEX);
 
     /**
-     * Valida si un email tiene un formato válido.
-     * 
+     * Valida si un email tiene un formato valido.
+     *
      * @param email El email a validar.
-     * @return true si el email es válido, false en caso contrario.
+     * @return true si el email es valido, false en caso contrario.
      */
     public static boolean isValid(String email) {
         if (email == null || email.isEmpty()) {
-            return false; // Emails vacíos o nulos no son válidos
+            return false;
         }
         return EMAIL_PATTERN.matcher(email).matches();
     }
 
     /**
-     * Lanza una excepción si el email no es válido.
-     * 
+     * Lanza una excepcion si el email no es valido.
+     *
      * @param email El email a validar.
-     * @throws IllegalArgumentException Si el email no tiene un formato válido.
+     * @throws IllegalArgumentException Si el email no tiene un formato valido.
      */
     public static void validate(String email) {
         if (!isValid(email)) {
